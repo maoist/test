4f38bee3ccd86340431d5907dabacc15449978b1
guang_chen <chenjuqun@sina.com>
2015-12-28 16:54:09 +0800

    tttt

---
+++ src/com/guang/bio/TimeServer.java
@@ -0,0 +1,34 @@
+package com.guang.bio;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TimeServer {
+
+	public static void main(String[] args) throws IOException{
+
+		int port = 8080;
+		if (args != null && args.length > 0) {
+			port = Integer.valueOf(args[0]);
+		}
+		
+		ServerSocket server = null;
+		try {
+			server = new ServerSocket(port);
+			System.out.println("The time server is starting in port:"+port);
+			Socket socket = null;
+			while(true){
+				socket = server.accept();
+				new Thread(new TimeServerHandler(socket)).start();
+			}
+		} finally{
+			if(server != null){
+				server.close();
+				System.out.println("The time server is closed");
+				server = null;
+			}
+		}
+	}
+
+}
---
+++ src/com/guang/bio/TimeServer2.java
@@ -0,0 +1,35 @@
+package com.guang.bio;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TimeServer2 {
+
+	public static void main(String[] args) throws IOException{
+
+		int port = 8080;
+		if (args != null && args.length > 0) {
+			port = Integer.valueOf(args[0]);
+		}
+		
+		ServerSocket server = null;
+		try {
+			server = new ServerSocket(port);
+			System.out.println("The time server is starting in port:"+port);
+			Socket socket = null;
+			TimeServerHandlerExecutePool singleExecutor = new TimeServerHandlerExecutePool(50, 10000);
+			while(true){
+				socket = server.accept();
+				singleExecutor.execute(new TimeServerHandler(socket));
+			}
+		} finally{
+			if(server != null){
+				server.close();
+				System.out.println("The time server is closed");
+				server = null;
+			}
+		}
+	}
+
+}
